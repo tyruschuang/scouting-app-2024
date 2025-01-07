0a1e2a780bea34d00c79d4d468341432c3212294
Tyrus Chuang <62168528+tyruschuang@users.noreply.github.com>
2025-01-06 21:54:14 -0500

    Update README.md

--- src/firebase.js
+++ src/firebase.js
@@ -8,14 +8,14 @@ import {getFirestore} from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyC_hAOM34OqmtcmWIoGgQo8Fb45b4BPtQI",
-    authDomain: "scoutingapp-2024.firebaseapp.com",
-    databaseURL: "https://scoutingapp-2024-default-rtdb.firebaseio.com",
-    projectId: "scoutingapp-2024",
-    storageBucket: "scoutingapp-2024.appspot.com",
-    messagingSenderId: "715509081007",
-    appId: "1:715509081007:web:f43ad0e5ac2d295440455a",
-    measurementId: "G-S3H5BSJE3P"
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
 };
 
 // Initialize Firebase
